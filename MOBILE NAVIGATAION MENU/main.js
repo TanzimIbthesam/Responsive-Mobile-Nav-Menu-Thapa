a14fdf2deb382bde6c178fbb7db122cb0f7277d4
TanzimIbthesam <35595427+TanzimIbthesam@users.noreply.github.com>
2019-06-29 21:10:20 +0600

    Add files via upload

---
+++ MOBILE NAVIGATAION MENU/main.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var showhide=document.getElementById('myLinks');
+    if(showhide.style.display === "block"){
+        showhide.style.display="none";
+    }else{
+        showhide.style.display="block";
+    }
+}
